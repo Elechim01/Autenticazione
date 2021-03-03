82d47fbd8f7f2f4da4bc90d99da030db6101897b
Elechim01 <mikymanny015@gmail.com>
2021-03-03 16:34:42 +0100

    primo commit

--- Autenticazione/ContentView.swift
+++ Autenticazione/ContentView.swift
@@ -6,11 +6,37 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
+var screen = NSScreen.main!.visibleFrame
 struct ContentView: View {
+    @State var successo : Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(alignment: .center) {
+            if successo{
+                Text("Benvenuto")
+                .padding()
+            }
+           
+        }.onAppear{
+            autenticationUser()
+        }
+        .ignoresSafeArea(.all,edges: .all)
+        .frame(minWidth: 700, idealWidth: screen.width / 1.2, maxWidth: .infinity, minHeight: 600, idealHeight: screen.height - 60, maxHeight: .infinity)
+    }
+    func autenticationUser(){
+        let cotext = LAContext()
+        var error : NSError?
+        if cotext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "Login"
+            cotext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (succes, error) in
+                if succes {
+                    self.successo.toggle()
+                }else{
+                    print(error?.localizedDescription)
+                }
+            }
+        }
     }
 }
 
