db176ed5d53682a6c5f8609ac52717a7767f4ccf
Elechim01 <mikymanny015@gmail.com>
2021-03-03 16:03:33 +0100

    Initial Commit

---
+++ Autenticazione/AutenticazioneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutenticazioneApp.swift
+//  Autenticazione
+//
+//  Created by Michele Manniello on 03/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AutenticazioneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
